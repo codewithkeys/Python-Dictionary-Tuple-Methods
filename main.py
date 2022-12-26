f1817148692656b7a40034fbfa84d74a6081f1ad
Yoly Keys <116820221+codewithkeys@users.noreply.github.com>
2022-12-26 06:36:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# Python Dictionary Methods
+# Python has a set of built-in methods that you can use on dictionaries.
+
+# Method  Descriptions
+clear()	Removes all the elements from the dictionary
+copy()	Returns a copy of the dictionary
+fromkeys()	Returns a dictionary with the specified keys and value
+get()	Returns the value of the specified key
+items()	Returns a list containing a tuple for each key value pair
+keys()	Returns a list containing the dictionary's keys
+pop()	Removes the element with the specified key
+popitem()	Removes the last inserted key-value pair
+setdefault()	Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+update()	Updates the dictionary with the specified key-value pairs
+values()	Returns a list of all the values in the dictionary
+
+
+# Python Tuple Methods
+# Python has two built-in methods that you can use on tuples.
+
+# Method	Description
+count()	Returns the number of times a specified value occurs in a tuple
+index()	Searches the tuple for a specified value and returns the position of where it was found
